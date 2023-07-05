08984bd65805ff3703d893e3f7a5f31a1a046595
Giovanni Minghelli <105241670+giovannimin@users.noreply.github.com>
2023-07-05 18:58:04 +0200

    Update utils.py
    add new functions and add docstring

--- sources/utils.py
+++ sources/utils.py
@@ -6,9 +6,15 @@ Created on 25/05/2023 17:09
 
 import re
 import os
+from datetime import datetime, timedelta
 
 
 def get_root(root: str = "SmashTheOdds"):
+    """
+    Renvoie le chemin racine du projet spécifié.
+    :param root: (str) Nom de la racine du projet.
+    :return: (str) Chemin racine du projet.
+    """
     proj_path = os.getcwd()
     while os.path.basename(proj_path) != root:
         proj_path = os.path.dirname(proj_path)
@@ -18,10 +24,36 @@ def get_root(root: str = "SmashTheOdds"):
 
 def get_number_in_id(sr_id: str) -> str:
     """
-    IDs are encoded in Tennis API like (sr:competitor:983784, sr:tournament:87389, etc.)
-    Returns the ID without the prefix.
-    :param sr_id: (str) Encoded ID.
-    :return: (str) ID without prefix.
+    Les identifiants sont encodés dans l'API Tennis sous la forme (sr:competitor:983784, sr:tournament:87389, etc.)
+    Renvoie l'identifiant sans le préfixe.
+    :param sr_id: (str) Identifiant encodé.
+    :return: (str) Identifiant sans préfixe.
     """
     numbers = re.findall(r'\d+', sr_id)
     return numbers[0] if numbers else ""
+
+
+def check_file_modification(file_path, since: int = 3):
+    """
+    Vérifie si un fichier a été modifié au cours des "since" derniers jours.
+    :param file_path: (str) Chemin du fichier à vérifier.
+    :param since: (int) Nombre de jours à prendre en compte pour la vérification (par défaut: 3).
+    :return: (bool) True si le fichier a été modifié récemment, False sinon.
+    """
+    if os.path.isfile(file_path):
+        file_modified = datetime.fromtimestamp(os.path.getmtime(file_path))
+        three_days_ago = datetime.now() - timedelta(days=since)
+        if file_modified >= three_days_ago:
+            return True
+    return False
+
+
+def calculate_odds(probability):
+    """
+    Calcule les cotes à partir d'une probabilité de victoire.
+
+    :param probability: (float) Probabilité de victoire (entre 0 et 1).
+    :return: (float) Cotes.
+    """
+    odds = 1 / probability
+    return odds
