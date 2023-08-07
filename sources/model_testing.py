836e0629262608c2b40c2770f5a63877cebb5f72
Lu6D <lululanordic@hotmail.fr>
2023-08-07 11:24:00 +0200

    model_testing qui calcul l'accuracy et calculate_accuracy: git actions avec seuil d'alerte

--- sources/model_testing.py
+++ sources/model_testing.py
@@ -1,10 +1,20 @@
+
 import pandas as pd
-import numpy as np
-from sklearn.metrics import accuracy_score, precision_score, recall_score, confusion_matrix
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report, accuracy_score, confusion_matrix
+from sklearn.ensemble import RandomForestClassifier
+from sources.data_pipeline import global_transformer, next_events, history
 from sources.modelling import Model
 
+#récupération du modèle de ML de modelling.py
+mon_model= Model()
+mon_model.modelling()
 
-def show_accuracy():
-    accuracy= Model.get_accuracy()
-    return accuracy
+#définition de la variable weekly_accuracy qui sera l'accuracy calculer chaque semaine après avoir rechargé les données de l'API
+weekly_accuracy=mon_model.get_accuracy()
+#print(weekly_accuracy)
 
+# copie dans accuracy.txt
+with open("accuracy.txt", "w") as f:
+    f.write(str(weekly_accuracy))
+#dans git action, si l'accuracy est <0.6, une alerte est émise
